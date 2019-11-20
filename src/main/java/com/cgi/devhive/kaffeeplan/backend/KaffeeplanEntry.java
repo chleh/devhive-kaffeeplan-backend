e30ab879b310b49f1ef01c9be6e6db17da091efe
Christoph Lehmann <christoph.lehmann@cgi.com>
2019-11-20 15:43:34 +0100

    Spring Data REST backend now runs
    TODO: re-enable Swagger UI

--- src/main/java/com/cgi/devhive/kaffeeplan/backend/KaffeeplanEntry.java
+++ src/main/java/com/cgi/devhive/kaffeeplan/backend/KaffeeplanEntry.java
@@ -2,16 +2,18 @@ package com.cgi.devhive.kaffeeplan.backend;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
 @Entity
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class KaffeeplanEntry {
     @Id
-    final String yearWeek;
+    String yearWeek;
     String name;
     String email;
 }
