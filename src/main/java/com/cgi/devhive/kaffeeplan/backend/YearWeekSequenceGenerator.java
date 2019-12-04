da01568f9df650142e1f9a0bed70acc7bac34faa
Christoph Lehmann <christoph.lehmann@cgi.com>
2019-12-04 16:45:43 +0100

    Get max Primary Key from DB

--- src/main/java/com/cgi/devhive/kaffeeplan/backend/YearWeekSequenceGenerator.java
+++ src/main/java/com/cgi/devhive/kaffeeplan/backend/YearWeekSequenceGenerator.java
@@ -2,6 +2,7 @@ package com.cgi.devhive.kaffeeplan.backend;
 
 import org.hibernate.HibernateException;
 import org.hibernate.MappingException;
+import org.hibernate.engine.jdbc.connections.spi.ConnectionProvider;
 import org.hibernate.engine.spi.SharedSessionContractImplementor;
 import org.hibernate.id.enhanced.SequenceStyleGenerator;
 import org.hibernate.service.ServiceRegistry;
@@ -9,6 +10,7 @@ import org.hibernate.type.LongType;
 import org.hibernate.type.Type;
 
 import java.io.Serializable;
+import java.sql.*;
 import java.util.Properties;
 
 public class YearWeekSequenceGenerator extends SequenceStyleGenerator {
@@ -38,6 +40,40 @@ public class YearWeekSequenceGenerator extends SequenceStyleGenerator {
     public void configure(Type type, Properties params,
                           ServiceRegistry serviceRegistry) throws MappingException {
         super.configure(LongType.INSTANCE, params, serviceRegistry);
+
+        ConnectionProvider connProv = serviceRegistry.getService(ConnectionProvider.class);
+        Connection conn;
+        try {
+             conn = connProv.getConnection();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return;
+        }
+        CallableStatement stmt;
+        try {
+            stmt = conn.prepareCall("select max(year_Week) from Kaffeeplan_Entry;");
+            ResultSet rs = stmt.executeQuery();
+            while (rs.next()) {
+                System.out.println("RESULT: " + rs.getString(1));
+            }
+            // System.out.println("RESULT: " + stmt.getString(0));
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return;
+        }
+ /*
+        try {
+            PreparedStatement stmt;
+            stmt = conn.prepareStatement("select max(year_Week) from Kaffeeplan_Entry;");
+            stmt.execute();
+            stmt.
+            System.out.println("RESULT: " + stmt.getString(0));
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return;
+        }
+  */
+
     }
 
     /*
