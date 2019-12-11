cf7a1da99e2e9106d5ec363385da04af577c749b
Christoph Lehmann <christoph.lehmann@cgi.com>
2019-12-11 15:27:38 +0100

    Generate primary key

--- src/main/java/com/cgi/devhive/kaffeeplan/backend/YearWeekSequenceGenerator.java
+++ src/main/java/com/cgi/devhive/kaffeeplan/backend/YearWeekSequenceGenerator.java
@@ -11,80 +11,77 @@ import org.hibernate.type.Type;
 
 import java.io.Serializable;
 import java.sql.*;
+import java.util.Calendar;
 import java.util.Properties;
 
 public class YearWeekSequenceGenerator extends SequenceStyleGenerator {
-    //final String yearWeek =
+    int maxYearWeekY = -1;
+    int maxYearWeekW = -1;
 
     @Override
     public Serializable generate(SharedSessionContractImplementor session,
-                                 Object object) throws HibernateException {
-        return String.format("%06d", super.generate(session, object));
-        //return "XXX" + super.generate(session, object);
+                                 Object objectToBePersisted) throws HibernateException {
+        // String.format("%06d", super.generate(session, objectToBePersisted));
+        if (maxYearWeekW < 52) maxYearWeekW++;
+        else {
+            maxYearWeekW = 1;
+            maxYearWeekY++;
+        }
+
+        return String.format("%04d%02d", maxYearWeekY, maxYearWeekW);
     }
 
-    /*
-    @Override
-    public String getNext(String yearWeek) {
-        int number = Integer.parseInt(yearWeek);
-        int year = number/100;
-        int week = number%100;
-        if (week < 52) week++;
-        else {
-            week = 1;
-            year++;
-        }
-        return "" + year + (week < 10 ? "0" + week : week);
-    }*/
     @Override
     public void configure(Type type, Properties params,
                           ServiceRegistry serviceRegistry) throws MappingException {
         super.configure(LongType.INSTANCE, params, serviceRegistry);
+        init(serviceRegistry);
+    }
 
+    private void init(ServiceRegistry serviceRegistry)
+    {
         ConnectionProvider connProv = serviceRegistry.getService(ConnectionProvider.class);
         Connection conn;
+
         try {
-             conn = connProv.getConnection();
+            conn = connProv.getConnection();
         } catch (SQLException e) {
-            e.printStackTrace();
-            return;
+            throw new RuntimeException(e);
         }
-        CallableStatement stmt;
+
+        String maxYearWeek = null;
+
         try {
-            stmt = conn.prepareCall("select max(year_Week) from Kaffeeplan_Entry;");
+            CallableStatement stmt = conn.prepareCall(
+                    "select max(year_Week) from Kaffeeplan_Entry;");
             ResultSet rs = stmt.executeQuery();
             while (rs.next()) {
-                System.out.println("RESULT: " + rs.getString(1));
+                maxYearWeek = rs.getString(1);
+                System.out.println("RESULT: " + maxYearWeek);
             }
-            // System.out.println("RESULT: " + stmt.getString(0));
         } catch (SQLException e) {
-            e.printStackTrace();
-            return;
+            // SQLException might occur if the DB table does not exist, yet.
+            // E.g., if the H2 DB has just been created.
         }
- /*
-        try {
-            PreparedStatement stmt;
-            stmt = conn.prepareStatement("select max(year_Week) from Kaffeeplan_Entry;");
-            stmt.execute();
-            stmt.
-            System.out.println("RESULT: " + stmt.getString(0));
-        } catch (SQLException e) {
-            e.printStackTrace();
-            return;
-        }
-  */
 
+        if (maxYearWeek == null) {
+            int[] myw = getCurrentYearAndWeek();
+            maxYearWeekY = myw[0];
+            maxYearWeekW = myw[1];
+        } else {
+            int myw = Integer.parseInt(maxYearWeek);
+            maxYearWeekY = myw / 100;
+            maxYearWeekW = myw % 100;
+        }
     }
 
-    /*
-    @Override
-    public void configure(Type type, Properties params,
-                          ServiceRegistry serviceRegistry) throws MappingException {
-        super.configure(LongType.INSTANCE, params, serviceRegistry);
-        valuePrefix = ConfigurationHelper.getString(VALUE_PREFIX_PARAMETER,
-                params, VALUE_PREFIX_DEFAULT);
-        numberFormat = ConfigurationHelper.getString(NUMBER_FORMAT_PARAMETER,
-                params, NUMBER_FORMAT_DEFAULT);
+    private static int[] getCurrentYearAndWeek()
+    {
+        Calendar cal = Calendar.getInstance();
+        // ISO week: https://stackoverflow.com/a/147193
+        cal.setMinimalDaysInFirstWeek(4);
+        int year = cal.get(Calendar.YEAR);
+        int week = cal.get(Calendar.WEEK_OF_YEAR);
+        return new int[]{year, week};
     }
-     */
 }
