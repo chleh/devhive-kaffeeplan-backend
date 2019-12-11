9c8df45e1c33e24d40de099b966fedd6e30fbad3
Christoph Lehmann <christoph.lehmann@cgi.com>
2019-12-11 16:36:32 +0100

    Do not reschedule empty entries

--- src/main/java/com/cgi/devhive/kaffeeplan/backend/AutoRescheduleEntryTimer.java
+++ src/main/java/com/cgi/devhive/kaffeeplan/backend/AutoRescheduleEntryTimer.java
@@ -1,5 +1,6 @@
 package com.cgi.devhive.kaffeeplan.backend;
 
+import org.apache.logging.log4j.util.Strings;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,6 +27,9 @@ public class AutoRescheduleEntryTimer {
   }
 
   private void rescheduleUser(KaffeeplanEntry entry) {
+    if (Strings.isEmpty(entry.getName()) || Strings.isEmpty(entry.getEmail()))
+      return;
+
     KaffeeplanEntry newEntry = new KaffeeplanEntry();
     newEntry.setName(entry.getName());
     newEntry.setEmail(entry.getEmail());
