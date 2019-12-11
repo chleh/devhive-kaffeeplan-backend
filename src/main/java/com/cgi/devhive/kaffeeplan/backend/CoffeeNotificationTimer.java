9a781e7dad690783b33ba04742b7e53aa4e731d9
Francesco Freder <Francesco.Freder@cgi.com>
2019-12-11 15:31:45 +0100

    sending mails out of the timer

--- src/main/java/com/cgi/devhive/kaffeeplan/backend/CoffeeNotificationTimer.java
+++ src/main/java/com/cgi/devhive/kaffeeplan/backend/CoffeeNotificationTimer.java
@@ -2,6 +2,9 @@ package com.cgi.devhive.kaffeeplan.backend;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -10,9 +13,29 @@ public class CoffeeNotificationTimer {
 
   private static final Logger log = LoggerFactory.getLogger(CoffeeNotificationTimer.class);
 
-  //@Scheduled(fixedRate = 5000) every 5 seconds for testing purposes
+  @Autowired
+  public JavaMailSender emailSender;
+
+  //  @Scheduled(fixedRate = 5000) // every 5 seconds for testing purposes
   @Scheduled(cron = "0 0 12 * * WED") // every wednesday at 12
   public void notifyMembers() {
     log.info("Timer started about to notify the member about the task to do");
+
+    String eMailOfNextUser = getNextUser();
+    notifyUser(eMailOfNextUser);
+  }
+
+  private String getNextUser() {
+    return "benjamin.koehler@cgi.com";
+  }
+
+  private void notifyUser(String eMail) {
+    if (eMail != null) {
+      SimpleMailMessage message = new SimpleMailMessage();
+      message.setTo(eMail);
+      message.setSubject("Sie haben am Freitag Kaffeedienst");
+      message.setText("Mach ma");
+      emailSender.send(message);
+    }
   }
 }
